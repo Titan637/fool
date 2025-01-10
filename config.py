6fbaa081d87b86725c9e486beb56ea83336250e3
Titan637 <aliabbas729@hotmail.com>
2025-01-10 19:52:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "8022705558:AAFIkcDxK0w2HtkXfPKlraQRTqK4L63Mg-o"
+BOT_TOKEN = "7828525928:AAGZIUO4QnLsD_ITKGSkfN5NlGP3UZvU1OM"
 ADMIN_IDS = ["7163028849"]
-GROUP_ID = -1002175044331 # Replace with your group ID
+GROUP_ID = -1002298552334 # Replace with your group ID
 GROUP_LINK = "https://t.me/+8Uy26VLnS3A2ZTg1"  # Replace with your group invite link
 # Default number of threads for attacks
 DEFAULT_THREADS = 25  # Set the default number of threads to use for attacks
